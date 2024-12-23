e11bff2b3e3e653ebe36fa1faf8f8d154adaf456
Entrakit LLC <administratornikiruss@edenstreeacademy.com>
2024-12-23 05:01:25 -0600

    Create vit-config.js

---
+++ vit-config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/edens-tree-academy/',  // Replace with your repository name
+})
